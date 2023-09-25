0fb697eaabe847d82ad9d4bf3b03599fdddbfd2f
Lewis Kai Ho Wong (k22027157) <lewis.wong@kcl.ac.uk>
2023-09-25 14:43:50 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if item in frequencies:
+            count = frequencies[item]
+            frequencies.update({item: count + 1})
+        else:
+            frequencies[item] = 1
+
     return frequencies
+
+print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
